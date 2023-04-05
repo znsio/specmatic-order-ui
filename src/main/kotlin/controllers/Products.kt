5c8dc0d9da3005123d22badda0841c90a59e9868
Naresh Jain <nashjain@gmail.com>
2023-04-05 18:47:47 +0530

    moved api url from Products.kt to application.properties

--- src/main/kotlin/controllers/Products.kt
+++ src/main/kotlin/controllers/Products.kt
@@ -2,6 +2,7 @@ package controllers
 
 import org.json.JSONArray
 import org.json.JSONObject
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.*
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.client.RestTemplate
@@ -9,8 +10,6 @@ import java.net.URI
 
 data class PetInfo(val id: Int, val name: String)
 data class CreationStatus(val id: Int, val status: String)
-
-const val base = "http://localhost:9000"
 
 enum class API(val method: HttpMethod, val url: String) {
     CREATE_ORDER(HttpMethod.POST, "/orders")
@@ -22,15 +21,23 @@ data class ProductDetails(val id: Int, val name: String) {
 
 @RestController
 class WebsiteAPI {
+    @Value("\${order.api}")
+    lateinit var orderAPIUrl: String
+
     @PostMapping("/orders", produces = [MediaType.APPLICATION_JSON_VALUE])
     fun createOrder(@RequestBody order: String): CreationStatus {
         val orderJSON = JSONObject(order)
         orderJSON.put("status", "pending")
-        return CreationStatus(id = callCreateOrderAPI(orderJSON.toString()), status="success")
+        return CreationStatus(id = callCreateOrderAPI(orderJSON.toString()), status = "success")
     }
 
     @GetMapping("/findAvailableProducts", produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun findAvailableProducts(@RequestParam(name="type", required=true) type: String): ResponseEntity<List<ProductDetails>> {
+    fun findAvailableProducts(
+        @RequestParam(
+            name = "type",
+            required = true
+        ) type: String
+    ): ResponseEntity<List<ProductDetails>> {
         try {
             val result = getFromAPI("/products?type=$type")
             val products = JSONArray(result)
@@ -40,36 +47,37 @@ class WebsiteAPI {
             }
 
             return ResponseEntity(availableProducts, HttpStatus.OK)
-        } catch(e: Throwable) {
+        } catch (e: Throwable) {
             return ResponseEntity(HttpStatus.NOT_FOUND)
         }
     }
 
-    private fun getFromAPI(url: String): String = RestTemplate().getForEntity(URI.create("$base$url"), String::class.java).body?.trim() ?: ""
+    private fun getFromAPI(url: String): String =
+        RestTemplate().getForEntity(URI.create("$orderAPIUrl$url"), String::class.java).body?.trim() ?: ""
+
+    private fun callCreateOrderAPI(order: String): Int {
+        return writeJSONToAPI(API.CREATE_ORDER, order)?.let {
+            JSONObject(it).getInt("id")
+        } ?: error("No order id received in response to create request.")
+    }
+
+    private fun writeJSONToAPI(api: API, body: String): String? {
+        val uri = URI.create("$orderAPIUrl${api.url}")
+        val headers = HttpHeaders()
+        headers["Content-Type"] = "application/json"
+        val request = RequestEntity(body, headers, api.method, uri)
+        val response = RestTemplate().exchange(request, String::class.java)
+        return response.body
+    }
+
+    fun getAuthToken(username: String, password: String): String {
+        val uri = URI.create("$orderAPIUrl/auth")
+        val headers = HttpHeaders()
+        headers["Content-Type"] = "application/json"
+
+        val request =
+            RequestEntity("{\"username\": \"$username\", \"password\": \"$password\"}", headers, HttpMethod.POST, uri)
+        val response = RestTemplate().exchange(request, String::class.java)
+        return response.body ?: ""
+    }
 }
-
-private fun callCreateOrderAPI(order: String): Int {
-    return writeJSONToAPI(API.CREATE_ORDER, order)?.let {
-        JSONObject(it).getInt("id")
-    } ?: error("No order id received in response to create request.")
-}
-
-private fun writeJSONToAPI(api: API, body: String): String? {
-    val uri = URI.create("$base${api.url}")
-    val headers = HttpHeaders()
-    headers["Content-Type"] = "application/json"
-    val request = RequestEntity(body, headers, api.method, uri)
-    val response = RestTemplate().exchange(request, String::class.java)
-    return response.body
-}
-
-fun getAuthToken(username: String, password: String): String {
-    val uri = URI.create("$base/auth")
-    val headers = HttpHeaders()
-    headers["Content-Type"] = "application/json"
-
-    val request = RequestEntity("{\"username\": \"$username\", \"password\": \"$password\"}", headers, HttpMethod.POST, uri)
-    val response = RestTemplate().exchange(request, String::class.java)
-    return response.body ?: ""
-}
-
